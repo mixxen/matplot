60339528caca212f249340a3df2ed9be9f1f7e14
alexander.cabello@gmail.com <alexander.cabello@gmail.com>
2014-06-04 10:38:58 -1000

    Bower updates

--- demos/demo.py
+++ demos/demo.py
@@ -20,8 +20,8 @@ templateDef='''
    <link href="prettify/prettify.css" type="text/css" rel="stylesheet" />
    <script type="text/javascript" src="prettify/prettify.js"></script>
    <script type="text/javascript" src="numeric-1.2.3.js"></script>
-   <link href="../matplot.css" rel="stylesheet" type="text/css" />
-   <script type="text/javascript" src="matplot.js"></script>
+   <link href="../dist/matplot.css" rel="stylesheet" type="text/css" />
+   <script type="text/javascript" src="../dist/matplot.js"></script>
 $head
 
    <script type="text/javascript">
